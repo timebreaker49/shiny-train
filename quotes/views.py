8182652bc50b86eccfbcba09334e7f0cffe99154
timebreaker49 <wcsbaylor@gmail.com>
2022-12-22 13:42:10 -0500

    Added tag filter function in Quote viewset class, tested in terminal

--- quotes/views.py
+++ quotes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
 from .models import Category, Quote
 from rest_framework import viewsets
+from rest_framework.decorators import action, api_view
+from rest_framework.response import Response
 from .serializers import QuoteSerializer
 from .authentication import CsrfExemptSessionAuthentication, BasicAuthentication
 # Create your views here.
@@ -12,6 +15,7 @@ def add_to_category(request, quote_id):
     quote = get_object_or_404(Quote, pk=quote_id)
     return HttpResponse("quote: %s" % quote)
 
+
 class IndexView(generic.ListView):
     template_name = 'quotes/index.html'
     context_object_name = 'categories'
@@ -19,13 +23,23 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return 5 categories in alphabetical order"""
         return Category.objects.order_by('-name')[:5]
-    
+
+
 class DetailView(generic.DetailView):
     model = Category
     template_name = 'quotes/detail.html'
-    
+
+
 class QuoteViewSet(viewsets.ModelViewSet):
     serializer_class = QuoteSerializer
     queryset = Quote.objects.all()
     # Temporary solution to prevent csrf checks during API requests
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
+    @api_view(['GET'])
+    @csrf_exempt
+    def filter_for_tags(request):
+        params = request.data['tags']
+        quotes = Quote.objects.filter(tags__name__in=params).distinct()
+        serializer = QuoteSerializer(quotes, many=True)
+        return Response(serializer.data, status=200)
--- quotesdjango/urls.py
+++ quotesdjango/urls.py
@@ -7,8 +7,10 @@ from quotes import views
 router = routers.DefaultRouter()
 router.register(r'quotes', views.QuoteViewSet, 'quotes')
 
+
 urlpatterns = [
     path('api/', include(router.urls)),
     path('quotes/', include('quotes.urls')),
+    path('tagged/', views.QuoteViewSet.filter_for_tags, name='tagged'),
     path('admin/', admin.site.urls),
 ]
