7c1dc4e035c3432070d14419e3850e9dffa689cd
timebreaker49 <wcsbaylor@gmail.com>
2023-01-03 15:46:55 -0500

    Parsing soup data into array of { quote, author }

--- quotesdjango/scrape.py
+++ quotesdjango/scrape.py
@@ -5,9 +5,30 @@ import re
 URL = "https://blog.hubspot.com/sales/famous-quotes"
 page = requests.get(URL)
 
-regex = r'"<p>[\n\r\s]+[0-9]+. .+"[\n\r\s]+<em>[\n\r\s]+-.+[\n\r\s]+<\/em>[\n\r\s]+<\/p>|<p>[\n\r\s]+[0-9]+.[' \
-        r'\n\r\s]+<em>[\n\r\s]+".+[\n\r\s]+<\/em>[\n\r\s]+<\/p>" '
-
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(soup.prettify())
+matches = soup.find_all(re.compile('p'))
+
+quotes = []
+
+for match in matches:
+    if re.match(r'[0-9]+\.', match.text): 
+        contents = match.contents
+        
+        # expected pattern where contents[0] = quote, contents[1] = author
+        if re.match(r'^-', contents[1].text):
+            quoteStart = contents[0].index('\"')
+            quoteEnd = contents[0].rfind('\"')
+            quotes.append({
+                'quote': str(match.contents[0][quoteStart+1:quoteEnd]),
+                'author': match.contents[1].text[1:]
+            })
+        # edge case where one quote wasn't formatted like the other quotes
+        else:
+            authorIndex = contents[1].text.index('-')
+            quotes.append({
+                'quote': contents[1].text[1:authorIndex],
+                'author': contents[1].text[authorIndex+1:]
+            })
+
+print(quotes)
