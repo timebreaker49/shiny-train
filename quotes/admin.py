667f409f2f582ca386bd2bb802935e2c277e3247
timebreaker49 <wcsbaylor@gmail.com>
2023-03-13 15:02:25 -0500

    deployment migration 2: moving around / removing old files

--- quotes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'get_quotes')
-
-    def get_quotes(self, obj):
-        return ",\n".join([q.quote for q in obj.quotes.all()])
-
