f77b7af2054dd843043201cbb347b72017884374
timebreaker49 <wcsbaylor@gmail.com>
2023-03-19 17:05:18 -0500

    added email to letsencrypt and fixed docker-compose

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(somerandomquotes.com www.somerandomquotes.com)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
+email="wcsbaylor@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
